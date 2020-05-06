84abae8fd245148a67193e8e96c5f2654f3840a8
kanishkasahu <kanishkasahu005@gmail.com>
2020-05-06 15:59:47 +0530

    splash screen

---
+++ app/src/main/java/com/example/liveweather/Splash.java
@@ -0,0 +1,24 @@
+package com.example.liveweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 3000);
+    }
+}
